6f8aa73e33b3dc9fbe82fd5fef3402488ed6cca2
nyt23 <3121763@STUD.SRH-CAMPUS-BERLIN.DE>
2024-06-09 19:08:27 +0200

    update

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -7,11 +7,13 @@ function Todo() {
     const [todos, setTodos] = useState([]);
     const [todo, setTodo] = useState('');
     const [time, setTime] = useState('');
+    const [date, setDate] = useState('');
 
     const handleAddTodo = () => { // Add todo function
-        setTodos([...todos, { todo, time }]);
+        setTodos([...todos, { todo, time, date }]);
         setTodo('');
         setTime('');
+        setDate('')
     }
 
 
@@ -26,16 +28,16 @@ function Todo() {
     return (
         <div>
             <h1>Todo List 📝</h1>
-            <input type='text' placeholder='Task' value={todo} onChange={(e) => setTodo(e.target.value)} />
-            <input type='time' placeholder='time' value={time} onChange={(e) => setTime(e.target.value)} />
-            <input type='date' placeholder='date' value={time} onChange={(e) => setTime(e.target.value)} />
+            <input type='text' placeholder='My Task' onChange={(e) => setTodo(e.target.value)} />
+            <input type='time' value={time} onChange={(e) => setTime(e.target.value)} />
+            <input type='date' value={date} onChange={(e) => setTime(e.target.value)} />
             <button onClick={handleAddTodo}>Add</button>
             <ul>
                 {todos.map((todo, index) => (
                     <div className='list' key={index}>
                         <input type='checkbox'
                                onClick={() => handleRemoveTodo(index)} />
-                        {todo.todo} - {todo.time} - {todo.date}
+                        {todo.todo} - {todo.time} on {todo.date}
                     </div>
                 ))}
             </ul>
